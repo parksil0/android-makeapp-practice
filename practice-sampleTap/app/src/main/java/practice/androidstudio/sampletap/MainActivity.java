7f9969180dc4faec2a7a19d22872f48b87a75b64
parksil <parksil00@naver.com>
2020-11-26 18:29:47 +0900

    20201126

--- practice-sampleTap/app/src/main/java/practice/androidstudio/sampletap/MainActivity.java
+++ practice-sampleTap/app/src/main/java/practice/androidstudio/sampletap/MainActivity.java
@@ -3,13 +3,15 @@ package practice.androidstudio.sampletap;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.material.tabs.TabLayout;
 
-
 public class MainActivity extends AppCompatActivity {
+
     Toolbar toolbar;
 
     Fragment1 fragment1;
@@ -36,5 +38,35 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.addTab(tabs.newTab().setText("통화기록"));
+        tabs.addTab(tabs.newTab().setText("스팸기록"));
+        tabs.addTab(tabs.newTab().setText("연락처"));
+
+        tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                Log.d("MainActivity", "선택된 택 : " + position);
+                Fragment selected = null;
+                if(position == 0) {
+                    selected = fragment1;
+                } else if(position ==1) {
+                    selected = fragment2;
+                } else if(position ==2) {
+                    selected = fragment3;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, selected).commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
